e81e7d281f27ca16474086e4f4220701447e48b5
msantor <mehmet.santor@orioninc.com>
2023-01-08 16:19:55 +0300

    Unit tests

--- bookshop/src/test/java/com/msantor/bookshop/mapper/ClientMapperTest.java
+++ bookshop/src/test/java/com/msantor/bookshop/mapper/ClientMapperTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
